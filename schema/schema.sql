6e7a94c06e804030417f13a55c24b378aa16f5ec
sapitorico <renzoyaques@gmail.com>
2024-03-18 22:26:38 -0300

    add stored procedure Add_to_cart

--- schema/schema.sql
+++ schema/schema.sql
@@ -364,6 +364,39 @@ END$$
 DELIMITER ;
 
 -- -----------------------------------------------------
+-- procedure Add_to_cart
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `ecommerce_db`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Add_to_cart`(
+    IN p_product_id VARCHAR(36),
+    IN p_user_id VARCHAR(36),
+    IN p_quantity INT,
+    IN p_cart_id VARCHAR(36)
+)
+BEGIN    
+    DECLARE v_id VARCHAR(36);
+    DECLARE current_quantity INT;
+
+    IF NOT EXISTS (SELECT id FROM products WHERE id = p_product_id) THEN
+        SELECT 'not_exist' AS message;
+    ELSE
+        SELECT id, quantity INTO v_id, current_quantity FROM cart WHERE customer_id = p_user_id AND product_id = p_product_id;
+        IF v_id IS NOT NULL THEN
+            UPDATE cart SET quantity = current_quantity + p_quantity WHERE id = v_id;
+            SELECT 'success' AS message;
+        ELSE
+            INSERT INTO cart (id, customer_id, product_id, quantity)
+            VALUES (p_cart_id, p_user_id , p_product_id, p_quantity);
+            SELECT 'success' AS message;
+        END IF;
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
 -- procedure Get_cart
 -- -----------------------------------------------------
 
